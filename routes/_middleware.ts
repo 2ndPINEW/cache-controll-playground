3eef80fdac13bbe65154ca58161097c981c271b7
Eito Takase <eito01178@gmail.com>
2023-04-02 12:23:32 +0900

    s-maxage

---
+++ routes/_middleware.ts
@@ -0,0 +1,38 @@
+import { MiddlewareHandlerContext } from "$fresh/server.ts";
+import { createApiErrorString } from "../util/error.ts";
+
+interface State {
+  data: string;
+}
+
+// 404エラーが起きた時、APIの場合はエラーJSONを返し、それ以外の場合はindex.htmlを返すハンドラ
+function fallbackHandler(req: Request, resp: Response) {
+  if (req.url.includes("/api/")) {
+    const error = createApiErrorString({
+      message: "Not found",
+      status: 404,
+    });
+    return new Response(error, {
+      status: 404,
+    });
+  } else {
+    return new Response(Deno.readFileSync(`${Deno.cwd()}/static/index.html`), {
+      status: 200,
+      headers: resp.headers,
+    });
+  }
+}
+
+export async function handler(
+  req: Request,
+  ctx: MiddlewareHandlerContext<State>
+) {
+  const resp = await ctx.next();
+  if (resp.status === 404) {
+    return fallbackHandler(req, resp);
+  }
+  await new Promise(resolve => setTimeout(resolve, 3000));
+  resp.headers.append('max-age', '0')
+  resp.headers.append('s-maxage', '120')
+  return resp;
+}
